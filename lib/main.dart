d7a86f4853e307dd2df94583a47dbd6633c425ea
Ken Majas Islamy <anjayym639@gmail.com>
2025-08-18 11:38:32 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:employment_attendance/feature/presentation/page/edit_profile_page.dart';
-import 'package:employment_attendance/feature/presentation/page/login_page.dart';
-import 'package:employment_attendance/feature/presentation/page/register_page.dart';
 import 'package:flutter/material.dart';
-import 'feature/presentation/page/dashboard_page.dart';
-import 'feature/presentation/page/profile_page.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +10,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      title: 'Absensi App',
       debugShowCheckedModeBanner: false,
-      title: 'Employment App',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primarySwatch: Colors.green,
-        fontFamily: 'Roboto',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  // variabel reaktif pakai GetX
+  final counter = 0.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Absensi App")),
+      body: Center(
+        child: Obx(() => Text(
+              "Counter: ${counter.value}",
+              style: const TextStyle(fontSize: 20),
+            )),
       ),
-      home: const ProfilePage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter.value++, // tambah counter
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
