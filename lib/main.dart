3848589b8263421c7a59087e9b8556bf55073057
Raffy626 <reshainapasha@gmail.com>
2025-08-18 13:39:25 +0700

    refactor: add LmsPage and update main.dart imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'feature/presentation/page/dashboard_page.dart';
+import 'feature/presentation/page/lms_page.dart';
+import 'feature/presentation/page/login_page.dart';
+import 'feature/presentation/page/register_page.dart';
+import 'feature/presentation/page/notification_page.dart';
 
 void main() {
   runApp(const MyApp());
